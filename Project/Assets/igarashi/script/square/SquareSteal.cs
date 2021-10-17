34bad59e4b2fd231d567bebc5fa1fa4c4efed9ec
igarashi-ryo <55534019+igarashi-ryo@users.noreply.github.com>
2021-10-17 17:40:19 +0900

    [fix]SquareSteal お土産を持っていない人や守られている人を選んだときのバグ修正

--- Project/Assets/igarashi/script/square/SquareSteal.cs
+++ Project/Assets/igarashi/script/square/SquareSteal.cs
@@ -22,6 +22,7 @@ public class SquareSteal : SquareBase
     StatusWindow _statusWindow;
     PayUI _payUI;
     SelectUI _selectUI;
+    List<string> _selectElements;
 
     private List<CharacterBase> _otherCharacters;
 
@@ -35,6 +36,7 @@ public class SquareSteal : SquareBase
         _statusWindow = FindObjectOfType<StatusWindow>();
         _payUI = FindObjectOfType<PayUI>();
         _selectUI = FindObjectOfType<SelectUI>();
+        _selectElements = new List<string>();
     }
 
     // Update is called once per frame
@@ -79,6 +81,8 @@ public class SquareSteal : SquareBase
         _otherCharacters.AddRange(FindObjectsOfType<CharacterBase>());
         _otherCharacters.Remove(_character.GetComponent<CharacterBase>());
 
+        _selectElements.Clear();
+
         _state = SquareStealState.PAY;
     }
 
@@ -88,12 +92,11 @@ public class SquareSteal : SquareBase
         {
             if (_payUI.IsSelectYes())
             {
-                List<string> names = new List<string>();
-                names.Add("Ç‚ÇﬂÇÈ");
                 for (int i = 0; i < _otherCharacters.Count; i++)
-                    names.Add(_otherCharacters[i].Name);
+                    _selectElements.Add(_otherCharacters[i].Name);
+                _selectElements.Add("Ç‚ÇﬂÇÈ");
 
-                _selectUI.Open(names);
+                _selectUI.Open(_selectElements);
                 _messageWindow.SetMessage("íNÇ©ÇÁÇ®ìyéYÇíDÇ¢Ç‹Ç∑Ç©ÅH", _character.IsAutomatic);
 
                 _state = SquareStealState.SLECT_TARGET;
@@ -109,22 +112,27 @@ public class SquareSteal : SquareBase
 
     private void SelectTargetStateProcess()
     {
+        
         if(_selectUI.IsComplete && !_messageWindow.IsDisplayed)
         {
-            if(_selectUI.SelectIndex == 0)
+            if(_selectUI.SelectIndex == _otherCharacters.Count)
             {
                 _state = SquareStealState.END;
                 return;
             }
 
-            if(_otherCharacters[_selectUI.SelectIndex].gameObject.GetComponent<Protector>().IsProtected)
+            if (_otherCharacters[_selectUI.SelectIndex].gameObject.GetComponent<Protector>().IsProtected)
             {
                 _messageWindow.SetMessage(_otherCharacters[_selectUI.SelectIndex].Name + "ÇÕêgÇéÁÇÁÇÍÇƒÇ¢ÇÈ", _character.IsAutomatic);
+                _selectUI.Open(_selectElements);
+
                 return;
             }
             else if(_otherCharacters[_selectUI.SelectIndex].Souvenirs.Count == 0)
             {
-                _messageWindow.SetMessage(_otherCharacters[_selectUI.SelectIndex].Name + "ÇÕÇ®ìyéYÇéùÇ¡ÇƒÇ¢Ç»Ç¢ÅI", _character.IsAutomatic);
+                _messageWindow.SetMessage(_otherCharacters[_selectUI.SelectIndex].Name + "Ç®ìyéYÇéùÇ¡ÇƒÇ¢Ç»Ç¢", _character.IsAutomatic);
+                _selectUI.Open(_selectElements);
+
                 return;
             }
             else
