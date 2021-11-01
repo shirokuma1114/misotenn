b807d6fd0397b19fe60d7e11218b7056eca22351
igarashi-ryo <55534019+igarashi-ryo@users.noreply.github.com>
2021-11-01 17:48:14 +0900

    [update]マスの線の頂点数、マスからどのくらい浮かすかを設定できるように

--- Project/Assets/igarashi/script/square/Line/SquareConnectionLine.cs
+++ Project/Assets/igarashi/script/square/Line/SquareConnectionLine.cs
@@ -7,6 +7,10 @@ public class SquareConnectionLine : MonoBehaviour
 {
     private LineRenderer _lineRenderer;
 
+    [SerializeField]
+    private float _floatingVolume = 0.3f;
+    [SerializeField]
+    private int _curvePointNum = 10;
 
     // Start is called before the first frame update
     void Awake()
@@ -35,36 +39,37 @@ public class SquareConnectionLine : MonoBehaviour
 
         var japan = GameObject.Find("Japan");
         Vector3 start = transform.InverseTransformPoint(japan.transform.position);
-        start *= 1.05f;
+        start += start.normalized * _floatingVolume;
+        positions.Add(start);
 
         GameObject endSquare = japan.GetComponent<SquareBase>().OutConnects[0].gameObject;
         Vector3 end = transform.InverseTransformPoint(endSquare.transform.position);
-        end *= 1.05f;
+        end += end.normalized * _floatingVolume;
 
         for (int i = 0; i < squares.Count; i++)
         {
-            for (float t = 0; t <= 1.1f; t += 0.1f)
+            for (int t = 1; t <= _curvePointNum; t++)
             {
-                var p = Vector3.Slerp(start, end, t);
+                var p = Vector3.Slerp(start, end, (float)t /_curvePointNum);
                 positions.Add(p);
             }
 
             start = end;
             endSquare = endSquare.GetComponent<SquareBase>().OutConnects[0].gameObject;
             end = transform.InverseTransformPoint(endSquare.transform.position);
-            end *= 1.05f;
+            end += end.normalized * _floatingVolume;
         }
 
         _lineRenderer.positionCount = positions.Count;
         _lineRenderer.SetPositions(positions.ToArray());
     }
 
-    private Vector3 SampleCurve(Vector3 start, Vector3 end, Vector3 control, float t)
+    private Vector3 BezierLerp(Vector3 start, Vector3 end, Vector3 control, float t)
     {
-        Vector3 Q0 = Vector3.Lerp(start, control, t);
-        Vector3 Q1 = Vector3.Lerp(control, end, t);
-        Vector3 Q2 = Vector3.Lerp(Q0, Q1, t);
+        Vector3 sc = Vector3.Lerp(start, control, t);
+        Vector3 ce = Vector3.Lerp(control, end, t);
 
-        return Q2;
+        Vector3 ret = Vector3.Lerp(sc, ce, t);        
+        return ret;
     }
 }
