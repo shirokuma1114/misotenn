b645655769762d0e97ca72bd83f6a638221066b4
Zaitsu_Hiromu <hiromu.zai@gmail.com>
2021-11-18 10:48:06 +0900

    [add] 演出追加

--- Project/Assets/Zaitsu/Scripts/Horming.cs
+++ Project/Assets/Zaitsu/Scripts/Horming.cs
@@ -8,8 +8,8 @@ public class Horming : MonoBehaviour
     ParticleSystem.Particle[] m_Particles;
 
     // ターゲットをセットする
-    [SerializeField] private Transform target;
-    [SerializeField] private float _rotSpeed = 180.0f;  // 1秒間に回転する角度
+    //[SerializeField] private Transform target;
+    //[SerializeField] private float _rotSpeed = 180.0f;  // 1秒間に回転する角度
    
 
     void Start()
@@ -17,37 +17,36 @@ public class Horming : MonoBehaviour
         ps = GetComponent<ParticleSystem>();
     }
 
+    public float threshold = 100f;
+    public float intensity = 1f;
+
+    // ターゲットをセットする
+    public Transform target;
+
     void Update()
     {
         m_Particles = new ParticleSystem.Particle[ps.main.maxParticles];
         int numParticlesAlive = ps.GetParticles(m_Particles);
         for (int i = 0; i < numParticlesAlive; i++)
         {
-            var velocity = m_Particles[i].velocity;
-            var position = m_Particles[i].position;
+            var velocity = ps.transform.TransformPoint(m_Particles[i].velocity);
+            var position = ps.transform.TransformPoint(m_Particles[i].position);
 
-            // ターゲットへのベクトル
-            var direction = ps.transform.InverseTransformPoint(target.TransformPoint(target.position)) - position;
+            var period = m_Particles[i].remainingLifetime * 0.9f;
 
-            // ターゲットまでの角度
-            float angleDiff = Vector3.Angle(velocity, direction);
+            //ターゲットと自分自身の差
+            var diff = target.position - position;
+            Vector3 accel = (diff - velocity * period) * 2f / (period * period);
 
-            // 回転角
-            float angleAdd = (_rotSpeed * Time.deltaTime);
-
-            // ターゲットへ向けるクォータニオン
-            Quaternion rotTarget = Quaternion.FromToRotation(velocity, direction);
-            if (angleDiff <= angleAdd)
+            //加速度が一定以上だと追尾を弱くする
+            if (accel.magnitude > threshold)
             {
-                // ターゲットが回転角以内なら完全にターゲットの方を向く
-                m_Particles[i].velocity = (rotTarget * velocity);
+                accel = accel.normalized * threshold;
             }
-            else
-            {
-                // ターゲットが回転角の外なら、指定角度だけターゲットに向ける
-                float t = (angleAdd / angleDiff);
-                m_Particles[i].velocity = Quaternion.Slerp(Quaternion.identity, rotTarget, t) * velocity;
-            }
+
+            // 速度の計算
+            velocity += accel * Time.deltaTime * intensity;
+            m_Particles[i].velocity = ps.transform.InverseTransformPoint(velocity);
         }
         ps.SetParticles(m_Particles, numParticlesAlive);
     }
