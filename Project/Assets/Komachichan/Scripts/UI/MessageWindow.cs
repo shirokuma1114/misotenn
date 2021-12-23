543ade1331bef5269552e563d97450571960cea1
komachi114 <oreoresuke@gmail.com>
2021-12-23 14:10:12 +0900

    ぷる

--- Project/Assets/Komachichan/Scripts/UI/MessageWindow.cs
+++ Project/Assets/Komachichan/Scripts/UI/MessageWindow.cs
@@ -54,6 +54,8 @@ public class MessageWindow : WindowBase
 
     private float _iconPosY;
 
+    private bool _playerInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -96,6 +98,11 @@ public class MessageWindow : WindowBase
             _iconTr.anchoredPosition = pos;
 
         }
+
+        if(_playerInput && (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("A")))
+        {
+            SetNextMessage();
+        }
         if (!_isAutomatic && (Input.GetKeyDown(KeyCode.Return) || _character.Input.GetButtonDown("A")))
         {
             SetNextMessage();
@@ -120,7 +127,7 @@ public class MessageWindow : WindowBase
     }
 
     // ‹æØ‚è•¶š‚ğ<>‚Æ‚·‚é
-    public void SetMessage(string message, CharacterBase character)
+    public void SetMessage(string message, CharacterBase character, bool playerInput = false)
     {
         _isDisplayed = true;
         _splitMessage = Regex.Split(message, @"\s*" + _splitString + @"\s*", RegexOptions.IgnorePatternWhitespace);
@@ -133,6 +140,7 @@ public class MessageWindow : WindowBase
         _text.enabled = true;
         _isAutomatic = character.IsAutomatic;
         _character = character;
+        _playerInput = playerInput;
     }
 
     public void SetTextSpeed(float textSpeed)
